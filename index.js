688143db33a6fa27f3c94042bacc079e399d4016
RayhanalKavey <rayhanalkavey@gmail.com>
2022-11-24 04:29:47 +0600

    Connect to the mongodb by protecting secret keys.

--- index.js
+++ index.js
@@ -11,9 +11,31 @@ const port = process.env.PORT || 5005;
 app.use(cors());
 app.use(express.json());
 
+// Db connections
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hufticd.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+// client.connect((err) => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
+///===================================
+async function run() {
+  try {
+  } finally {
+  }
+}
+run().catch(console.dir);
+///===================================
+
 app.get("/", (req, res) => {
-  res.send("data is coming");
+  res.send("Welcome to Laptop Utopia");
 });
 app.listen(port, () => {
-  console.log(`Server is running on port: ${port}`);
+  console.log(`Laptop Utopia server is running on port: ${port}`);
 });
